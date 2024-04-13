4251e95ccd7844fa7ec8697d1b354e59c6a04fe8
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2024-04-13 08:47:22 +0200

    add: client - server folder

---
+++ server/connessione.php
@@ -0,0 +1,31 @@
+<?php
+//  	Connessione al DBMS e selezione del database.
+//
+// 	nome dell'host
+//
+$host = "localhost";
+//
+// 	username dell'utente in connessione
+//
+$user = "root";
+//
+// 	password dell'utente
+//
+$password = "";
+//
+// nome del database
+//
+$db = "codici_postali";
+//
+// 	Istanza dell'oggetto della classe MySQLi
+//
+$connessione = new mysqli($host, $user, $password, $db);
+//
+// 	Verifica su eventuali errori di connessione
+//
+if ($connessione->connect_errno)
+{
+    echo("Connessione fallita: ".$connessione->connect_error.".");
+    exit();
+}
+?>
